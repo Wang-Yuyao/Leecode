fbb210a8340b2b74f3dc253480de5e65f6262f0a
wang.yuyao <wang.yuyao@systemi.co.jp>
2023-10-25 23:10:09 +0900

    add: 53

--- ProductOfArrayExceptSelf_238.java
+++ ProductOfArrayExceptSelf_238.java
@@ -4,7 +4,7 @@ public class ProductOfArrayExceptSelf_238 {
         int postfix = 1;
         int [] output = new int[nums.length];
         for (int index = 0; index < nums.length; index++) {
-            output[index] = prefix * nums[index];
+            output[index] = prefix;
             prefix = prefix * nums[index];
         }
         for (int index = nums.length-1; index >= 0; index--) {
